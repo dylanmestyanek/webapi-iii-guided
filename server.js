9538928221261bac83e9be838be78f5fd00c566a
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-10-30 12:29:38 -0500

    Updated gateKeeper middleware to check if a password doesn't exist, before checking to see if it equals 'mellon'

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-const dateLogger = (req, res, next) => {
-  console.log(new Date().toISOString())
+const logger = (req, res, next) => {
+  console.log(`MY CUSTOM LOGGER: [${new Date().toISOString()}] ${req.method} to ${req.path} from ${req.get('host')}`)
   next();
 }
 
-const pathLogger = (req, res, next) => {
-  console.log(`${req.method} to ${req.path}`)
-  next();
+const gateKeeper = (req, res, next) => {
+  // Data can come in the body, url parameters, query string, headers
+  // New way of reading data sent by client
+  const password = req.headers.password;
+
+  if (!password) {
+    res.status(400).json({ message: "Please provide a password." })
+  } else if (password.toLowerCase() === 'mellon') {
+    next();
+  } else {
+    res.status(401).json({ message: 'You shall not pass!'})
+  }
 }
 
 // Global Middleware
-server.use(helmet()) // Third party
+server.use(helmet()); // Third party
+server.use(gateKeeper);
 server.use(express.json()); // Built-in
-server.use(dateLogger) // Custom
-server.use(pathLogger) // Custom
+server.use(logger); // Custom
+server.use(morgan('dev')); // Third party
 
 server.use('/api/hubs', hubsRouter);
 
